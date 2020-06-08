078aa474209483ab292ba777ba1f920eaa92375e
Tia Simmons <t.simmons06@yahoo.com>
2020-06-08 19:01:51 -0400

    arrays

---
+++ C# Fundamentals/FruitsBasket/FruitsBasket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FruitsBasket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] fruit = { "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Apple", "Apple", "Apple", "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Apple", "Orange", "Orange" };
+            int apple = 0;
+            int orange = 0;
+
+            for(int i = 0; i < fruit.Length; i++)
+            {
+                if (fruit[i] == "Apple")
+                    apple++;
+                else if (fruit[i] == "Orange")
+                    orange++;
+                else
+                    break;
+            }
+
+            Console.WriteLine("Total # of Fruit in Basket: {0}", fruit.Length);
+            Console.WriteLine("Number of Apples {0}", apple);
+            Console.WriteLine("Number of Oranges {0}", orange);
+
+        }
+    }
+}
---
+++ C# Fundamentals/SimpleSort/SimpleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SimpleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstHalf = { 3, 7, 9, 10, 16, 19, 20, 34, 55, 67, 88, 99 };
+            int[] secondHalf = { 1, 4, 8, 11, 15, 18, 21, 44, 54, 79, 89, 100 };
+
+            int[] wholeNumbers = new int[24];
+
+            int firstHalfIndex = 0;
+            int secondHalfIndex = 0;
+
+            for (int i = 0; i < wholeNumbers.Length; i++)
+            {
+                if(firstHalfIndex >= firstHalf.Length)
+                {
+                    wholeNumbers[i] = secondHalf[secondHalfIndex];
+                    break;
+                }
+                if (secondHalfIndex >= secondHalf.Length)
+                {
+                    wholeNumbers[i] = firstHalf[firstHalfIndex];
+                    break;
+                }
+                if (firstHalf[firstHalfIndex] < secondHalf[secondHalfIndex])
+                {
+                    wholeNumbers[i] = firstHalf[firstHalfIndex];
+                    firstHalfIndex++;
+                }
+                else if(firstHalf[firstHalfIndex] > secondHalf[secondHalfIndex])
+                {
+                    wholeNumbers[i] = secondHalf[secondHalfIndex];
+                    secondHalfIndex++;
+                }
+            }
+
+            for (int i = 0; i < wholeNumbers.Length; i++)
+            {
+                Console.Write(wholeNumbers[i] + " " );
+            }
+        }
+    }
+}
