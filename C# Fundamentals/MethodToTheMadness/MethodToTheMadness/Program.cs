1c52de7085585478a58ba1222e7188f6f6a2ccd6
Tia Simmons <t.simmons06@yahoo.com>
2020-06-08 17:33:27 -0400

    methods

---
+++ C# Fundamentals/MethodToTheMadness/MethodToTheMadness/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MethodToTheMadness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EatMe();
+            DrinkMe();
+
+            Console.WriteLine("\n ? Lewis Carroll, Alice in Wonderland");
+        }
+        static void EatMe()
+        {
+            Console.WriteLine("'But I don’t want to go among mad people,' Alice remarked.");
+            Console.WriteLine("'Oh, you can’t help that,' said the Cat.");
+            Console.Write("'Oh, you can’t help that,' said the Cat.");
+        }
+
+        static void DrinkMe()
+        {
+            Console.WriteLine("'How do you know I’m mad?' said Alice.");
+            Console.WriteLine("'You must be,' said the Cat, 'or you wouldn’t have come here.'");
+        }
+    }
+}
