bdb874e5ebcf72e6f9cde5819ae65568eb58c6b5
Tia Simmons <t.simmons06@yahoo.com>
2020-07-20 17:45:12 -0400

    start menu and player name grabber created

---
+++ Classes and Objects/Final/Battleship/BattleShip.UI/ConsoleInput.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip.UI
+{
+    public class ConsoleInput
+    {
+        //gets player names from user input
+        //returns both player names in string array
+        public string[] GetPlayerNames()
+        {
+            string playerName;
+            string[] playerList = {null, null};
+            int playerCount = 1;
+
+            while (true)
+            {
+                Console.WriteLine("Enter the name for Player {0}", playerCount);
+                playerName = Console.ReadLine();
+                char firstLetter = playerName.ToCharArray().ElementAt(0);
+                if (char.IsLetter(firstLetter))
+                {
+                    playerList[playerCount - 1] = playerName;
+                    playerCount++;
+                }
+                if (playerList[1] != null)
+                    return playerList;
+            }
+        }
+    }
+}
